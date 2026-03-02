f4354da028537cae54f304a4ba8ba657f42941f9
Maximilian Hallqvist <98323670+MaximilianHq@users.noreply.github.com>
2026-03-02 22:23:30 +0100

    Add OTA update support to MCU1
    Introduce OTA capability via WiFi/ArduinoOTA. Added ota.h and ota.cpp implementing ota_begin(ssid, password) and ota_handle(), and integrated them into MCU1.ino (include ota.h, start WiFi/ArduinoOTA with hostname "esp32-agv", call ota_begin("QBit","internet") in setup and ota_handle() in loop). Also minor formatting/whitespace cleanup in MCU1.ino. Note: ota_begin blocks until WiFi connects.

--- AGV/MCU1/MCU1.ino
+++ AGV/MCU1/MCU1.ino
@@ -3,28 +3,35 @@
 #include "imu.h"
 #include "uwb.h"
 #include "types.h"
+#include "ota.h"
 
 #include <Arduino.h>
 #include <BluetoothSerial.h>
+#include <WiFi.h>
+#include <ArduinoOTA.h>
+
 BluetoothSerial SerialBT;
 
 const int UART_BAUD = 115200;
-const int BT_DELAY = 20000; // ms
 
 void setup()
 {
-    Serial.begin(UART_BAUD);     // PC
-    Serial1.begin(UART_BAUD);    // MCU2
-    Serial2.begin(UART_BAUD);    // DWM
-    Serial.setTimeout(30);       // PC
-    Serial1.setTimeout(30);      // MCU2
-    Serial2.setTimeout(30);      // DWM
+    Serial.begin(UART_BAUD);  // PC
+    Serial1.begin(UART_BAUD); // MCU2
+    Serial2.begin(UART_BAUD); // DWM
+    Serial.setTimeout(30);    // PC
+    Serial1.setTimeout(30);   // MCU2
+    Serial2.setTimeout(30);   // DWM
+
     SerialBT.begin("AGV_BT_G2"); // ÖS
     Serial.println("Bluethooth started");
+
+    ota_begin("QBit", "internet");
 }
 
 void loop()
 {
+    ota_handle();
     // read BT. packet: ös movement, ös command
     // $TCXXYYTTC\n or $TCCC\n
 
