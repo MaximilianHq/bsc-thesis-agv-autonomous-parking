04d20ac0f1f3333978b3fbcf07961e552f28d620
mayacoco4 <maria.wallbom04@gmail.com>
2026-03-27 14:02:06 +0100

    Update kalmanfilter.h
    ändringar

--- AGV/MCU1/kalmanfilter.h
+++ AGV/MCU1/kalmanfilter.h
@@ -1,10 +1,27 @@
 #pragma once
 #include "types.h"
 #include <Arduino.h>
+#include <math.h>
 
-void initMatrices(float dt);
-void predict();
-void update(float z[meas_dim]);
-void invert4x4(float A[4][4], float inv[4][4]);
+#define state_dim 6
+#define meas_dim 4
 
-float kalman_position(const ImuState &imu, const DwmState &dwm, int x, int y, int hz = 0.01)
+class KalmanFilter {
+
+    public:
+        float x[state_dim];
+        float P[state_dim][state_dim];
+        float A[state_dim][state_dim];
+        float H[meas_dim][state_dim];
+        float Q[state_dim][state_dim];
+        float R[meas_dim][meas_dim];
+        float K[state_dim][meas_dim];
+
+        KalmanFilter(float dt);
+        void initMatrices(float dt);
+        void predict();
+        void update(float z[meas_dim]);
+        void invert4x4(float A[4][4], float inv[4][4]);
+};
+
+float kalman_position(const ImuState &imu, const DwmState &dwm, int x, int y, int hz = 0.01);
