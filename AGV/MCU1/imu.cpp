d81eba24c65b4ebb79a9fede7f68155bc2f3509c
mayacoco4 <maria.wallbom04@gmail.com>
2026-04-07 15:00:24 +0200

    läsa imu data
    läsa imu data

--- AGV/MCU1/imu.cpp
+++ AGV/MCU1/imu.cpp
@@ -1,3 +1,61 @@
 #include "imu.h"
 #include "types.h"
 #include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+imuData::imuData() {}
+
+bool imuData::begin() {
+    Serial.println("Sensor test!");
+
+    if (!imu.begin()) {
+        Serial.println("Failed to find MPU6050 chip");
+        return false;
+    }
+
+    Serial.println("MPU6050 found!");
+
+    imu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    imu.setGyroRange(MPU6050_RANGE_500_DEG);
+    imu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+    printSettings();
+
+    return true;
+}
+
+void imuData::read_print() {
+    sensors_event_t a;
+    sensors_event_t g;
+    sensors_event_t temp;
+
+    imu.getEvent(&a, &g, &temp);
+
+    Serial.print("Acceleration X: ");
+    Serial.print(a.acceleration.x);
+    Serial.print(", Y: ");
+    Serial.print(a.acceleration.y);
+    Serial.print(", Z: ");
+    Serial.print(a.acceleration.z);
+    Serial.println(" m/s^2");
+
+    Serial.print("Rotation X: ");
+    Serial.print(g.gyro.x);
+    Serial.print(", Y: ");
+    Serial.print(g.gyro.y);
+    Serial.print(", Z: ");
+    Serial.print(g.gyro.z);
+    Serial.println(" rad/s");
+
+    Serial.print("Temperature: ");
+    Serial.print(temp.temperature);
+    Serial.println(" degC");
+
+    Serial.println("");
+}
+
+
+
+
--- AGV/MCU1/imu.h
+++ AGV/MCU1/imu.h
@@ -1,2 +1,22 @@
 #pragma once
 #include "types.h"
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+#ifndef MPU6050
+#define MPU6050
+
+class imuData {
+    public:
+        imuData();
+
+        bool begin();
+        void read_print();
+
+    private:
+    Adafruit_MPU6050 mpu;
+    void printSettings();
+};
+
+
