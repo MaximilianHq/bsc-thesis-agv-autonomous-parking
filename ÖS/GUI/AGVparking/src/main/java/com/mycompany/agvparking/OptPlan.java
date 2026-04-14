a3b70a8d5235ad1a86a17e292d02247b7fcf8999
hanbo337 <hanbo337@student.liu.se>
2026-04-14 09:32:13 +0200

    Demoknapp/startknapp

--- ÖS/GUI/AGVparking/src/main/java/com/mycompany/agvparking/OptPlan.java
+++ ÖS/GUI/AGVparking/src/main/java/com/mycompany/agvparking/OptPlan.java
@@ -219,11 +219,11 @@ public class OptPlan {
 
         double cost = 1;
 
-        // --- 2. Skapa Kanter (Edges) ---
+        // --- 2. Skapa Kanter (Edges)  ---
         for (int i = 0; i < ds.rows; i++) {
             for (int j = 0; j < ds.columns - 1; j++) {
                 cost = 1;
-                if (ds.ObstacleMatrix[j + 1][i] == 1) cost = 1000; // Hinder
+                if (ds.ObstacleMatrix[j + 1][i] == 1 || ds.ObstacleMatrix[j + 1][i] == 2) cost = 1000; // Hinder
 
                 // Horisontella
                 edges.add(new Edge("r", nodes.get(i * ds.columns + j), nodes.get(i * ds.columns + j + 1), cost));
@@ -232,13 +232,13 @@ public class OptPlan {
                 // Diagonala
                 if (i < ds.rows - 1) {
                     cost = 1.4;
-                    if (ds.ObstacleMatrix[j + 1][i + 1] == 1) cost = 1000;
+                    if (ds.ObstacleMatrix[j + 1][i + 1] == 1 || ds.ObstacleMatrix[j + 1][i + 1] == 2) cost = 1000;
                     edges.add(new Edge("dr", nodes.get(i * ds.columns + j), nodes.get((i + 1) * ds.columns + j + 1), cost));
                     edges.add(new Edge("ul", nodes.get((i + 1) * ds.columns + j + 1), nodes.get(i * ds.columns + j), cost));
                 }
                 if (i > 0) {
                     cost = 1.4;
-                    if (ds.ObstacleMatrix[j + 1][i - 1] == 1) cost = 1000;
+                    if (ds.ObstacleMatrix[j + 1][i - 1] == 1 || ds.ObstacleMatrix[j + 1][i - 1] == 2) cost = 1000;
                     edges.add(new Edge("ur", nodes.get(i * ds.columns + j), nodes.get((i - 1) * ds.columns + j + 1), cost));
                     edges.add(new Edge("dl", nodes.get((i - 1) * ds.columns + j + 1), nodes.get(i * ds.columns + j), cost));
                 }
@@ -249,7 +249,7 @@ public class OptPlan {
         for (int i = 0; i < ds.rows - 1; i++) {
             for (int j = 0; j < ds.columns; j++) {
                 cost = 1;
-                if (ds.ObstacleMatrix[j][i + 1] == 1) cost = 1000;
+                if (ds.ObstacleMatrix[j][i + 1] == 1 || ds.ObstacleMatrix[j][i + 1] == 2) cost = 1000;
                 
                 edges.add(new Edge("d", nodes.get(i * ds.columns + j), nodes.get((i + 1) * ds.columns + j), cost));
                 edges.add(new Edge("u", nodes.get((i + 1) * ds.columns + j), nodes.get(i * ds.columns + j), cost));
