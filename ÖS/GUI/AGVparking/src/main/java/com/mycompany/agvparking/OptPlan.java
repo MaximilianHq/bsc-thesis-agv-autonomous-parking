506ef94d395976f439b02be362ceefc1718194ca
frebe427 <frebe427@student.liu.se>
2026-04-24 15:02:20 +0200

    Ny rutt
    Rutten är nu olika beroende på om bil lastad eller ej - med bil på svänger den inte lika mycket då rotationen blir dyr. Mittenrad = "Highway" (kostar mindre än de andra) för att AGV ska hålla sig till mitten

--- ÖS/GUI/AGVparking/src/main/java/com/mycompany/agvparking/OptPlan.java
+++ ÖS/GUI/AGVparking/src/main/java/com/mycompany/agvparking/OptPlan.java
@@ -1,6 +1,3 @@
-/*
- * OptPlan.java
- */
 package com.mycompany.agvparking;
 
 import java.util.ArrayList;
@@ -17,12 +14,10 @@ public class OptPlan {
         this.ds = ds;
     }
 
-    private void buildGraph() {
-
+    private void buildGraph(boolean isLoaded) {
         nodes = new ArrayList<Vertex>();
         edges = new ArrayList<Edge>();
 
-        // --- 1. Skapa Noder ---
         for (int i = 0; i < ds.rows; i++) {
             for (int j = 0; j < ds.columns; j++) {
                 Vertex location = new Vertex("" + (i * ds.columns + j), "Nod #" + (i * ds.columns + j));
@@ -30,136 +25,89 @@ public class OptPlan {
             }
         }
 
-        double cost = 1;
+        int mainRow = (int) (ds.LocationY[0] / ds.gridsize);
+        double diagCost = isLoaded ? 2.1 : 1.4;
 
-// --- 2. Skapa Kanter (Edges)  ---
         for (int i = 0; i < ds.rows; i++) {
-            for (int j = 0; j < ds.columns - 1; j++) {
-
-                // Horisontella
-                cost = 1;
-                // Använder != 0 för att täcka in alla typer av hinder
-                if (ds.ObstacleMatrix[j + 1][i] != 0) {
-                    cost = 1000;
-                }
-                edges.add(new Edge("r", nodes.get(i * ds.columns + j), nodes.get(i * ds.columns + j + 1), cost));
-                edges.add(new Edge("l", nodes.get(i * ds.columns + j + 1), nodes.get(i * ds.columns + j), cost));
-
-                // Diagonala (Ner-Höger & Upp-Vänster)
-                if (i < ds.rows - 1) {
-                    cost = 1.4;
-                    // LÖSNING CORNER CLIPPING: Kolla målnoden OCH de två intilliggande rutorna!
-                    if (ds.ObstacleMatrix[j + 1][i + 1] != 0
-                            || ds.ObstacleMatrix[j + 1][i] != 0
-                            || ds.ObstacleMatrix[j][i + 1] != 0) {
-                        cost = 1000;
-                    }
-                    edges.add(new Edge("dr", nodes.get(i * ds.columns + j), nodes.get((i + 1) * ds.columns + j + 1), cost));
-                    edges.add(new Edge("ul", nodes.get((i + 1) * ds.columns + j + 1), nodes.get(i * ds.columns + j), cost));
-                }
-
-                // Diagonala (Upp-Höger & Ner-Vänster)
-                if (i > 0) {
-                    cost = 1.4;
-                    // LÖSNING CORNER CLIPPING: Kolla målnoden OCH de två intilliggande rutorna!
-                    if (ds.ObstacleMatrix[j + 1][i - 1] != 0
-                            || ds.ObstacleMatrix[j + 1][i] != 0
-                            || ds.ObstacleMatrix[j][i - 1] != 0) {
-                        cost = 1000;
-                    }
-                    edges.add(new Edge("ur", nodes.get(i * ds.columns + j), nodes.get((i - 1) * ds.columns + j + 1), cost));
-                    edges.add(new Edge("dl", nodes.get((i - 1) * ds.columns + j + 1), nodes.get(i * ds.columns + j), cost));
-                }
-            }
-        }
-
-        // --- 3. Skapa Vertikala Kanter ---
-        for (int i = 0; i < ds.rows - 1; i++) {
             for (int j = 0; j < ds.columns; j++) {
-                cost = 1;
-                if (ds.ObstacleMatrix[j][i + 1] == 1 || ds.ObstacleMatrix[j][i + 1] == 2) {
-                    cost = 1000;
+                int currentNode = i * ds.columns + j;
+
+                // --- HÖGER & VÄNSTER (Med Highway-rabatt) ---
+                if (j < ds.columns - 1) {
+                    double costR = (i == mainRow) ? 0.5 : 1.0;
+                    if (ds.ObstacleMatrix[j + 1][i] != 0) {
+                        costR = 1000.0;
+                    }
+                    edges.add(new Edge("r", nodes.get(currentNode), nodes.get(i * ds.columns + j + 1), costR));
+                }
+                if (j > 0) {
+                    double costL = (i == mainRow) ? 0.5 : 1.0;
+                    if (ds.ObstacleMatrix[j - 1][i] != 0) {
+                        costL = 1000.0;
+                    }
+                    edges.add(new Edge("l", nodes.get(currentNode), nodes.get(i * ds.columns + j - 1), costL));
                 }
 
-                edges.add(new Edge("d", nodes.get(i * ds.columns + j), nodes.get((i + 1) * ds.columns + j), cost));
-                edges.add(new Edge("u", nodes.get((i + 1) * ds.columns + j), nodes.get(i * ds.columns + j), cost));
+                // --- UPP & NER ---
+                if (i < ds.rows - 1) {
+                    double costD = (ds.ObstacleMatrix[j][i + 1] != 0) ? 1000.0 : 1.0;
+                    edges.add(new Edge("d", nodes.get(currentNode), nodes.get((i + 1) * ds.columns + j), costD));
+                }
+                if (i > 0) {
+                    double costU = (ds.ObstacleMatrix[j][i - 1] != 0) ? 1000.0 : 1.0;
+                    edges.add(new Edge("u", nodes.get(currentNode), nodes.get((i - 1) * ds.columns + j), costU));
+                }
+
+                // --- DIAGONALER ---
+                if (j < ds.columns - 1 && i < ds.rows - 1) {
+                    double costDR = diagCost;
+                    if (ds.ObstacleMatrix[j + 1][i + 1] != 0 || ds.ObstacleMatrix[j + 1][i] != 0 || ds.ObstacleMatrix[j][i + 1] != 0) {
+                        costDR = 1000.0;
+                    }
+                    edges.add(new Edge("dr", nodes.get(currentNode), nodes.get((i + 1) * ds.columns + j + 1), costDR));
+                }
+                if (j > 0 && i > 0) {
+                    double costUL = diagCost;
+                    if (ds.ObstacleMatrix[j - 1][i - 1] != 0 || ds.ObstacleMatrix[j - 1][i] != 0 || ds.ObstacleMatrix[j][i - 1] != 0) {
+                        costUL = 1000.0;
+                    }
+                    edges.add(new Edge("ul", nodes.get(currentNode), nodes.get((i - 1) * ds.columns + j - 1), costUL));
+                }
+                if (j < ds.columns - 1 && i > 0) {
+                    double costUR = diagCost;
+                    if (ds.ObstacleMatrix[j + 1][i - 1] != 0 || ds.ObstacleMatrix[j + 1][i] != 0 || ds.ObstacleMatrix[j][i - 1] != 0) {
+                        costUR = 1000.0;
+                    }
+                    edges.add(new Edge("ur", nodes.get(currentNode), nodes.get((i - 1) * ds.columns + j + 1), costUR));
+                }
+                if (j > 0 && i < ds.rows - 1) {
+                    double costDL = diagCost;
+                    if (ds.ObstacleMatrix[j - 1][i + 1] != 0 || ds.ObstacleMatrix[j - 1][i] != 0 || ds.ObstacleMatrix[j][i + 1] != 0) {
+                        costDL = 1000.0;
+                    }
+                    edges.add(new Edge("dl", nodes.get(currentNode), nodes.get((i + 1) * ds.columns + j - 1), costDL));
+                }
             }
         }
     }
 
-    // ÄNDRAD METOD: Tar nu startNode och endNode som parametrar
     public void createPlan(int startNodeId, int endNodeId) {
-        buildGraph();
-        // --- 4. Kör Dijkstra ---
+        buildGraph(true);
         Graph graph = new Graph(nodes, edges);
         DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(graph);
-
         LinkedList<Vertex> path = null;
-
         if (startNodeId >= 0 && startNodeId < nodes.size() && endNodeId >= 0 && endNodeId < nodes.size()) {
-
-            // Beräkna raksträckan (70 cm / gridsize 10 = 7 rutor)
-            int straightDistance = 7;
-            int entryNodeId = endNodeId - straightDistance;
-
-            // Kontrollera att infarten är på samma rad och inom kartan
-            if (entryNodeId >= 0 && (entryNodeId / ds.columns == endNodeId / ds.columns)) // Kör Dijkstra till punkten där raksträckan börjar
-            {
-                dijkstra.execute(nodes.get(startNodeId));
-                path = dijkstra.getPath(nodes.get(entryNodeId));
-
-                if (path != null) {
-                    // Lägg manuellt till de sista 7 rutorna fram till målet
-                    for (int k = 1; k <= straightDistance; k++) {
-                        path.add(nodes.get(entryNodeId + k));
-                    }
-
-                    System.out.println("Rutt hittad: " + path.size() + "steg");
-                }
-            }
-
-            if (path == null) {
-                // Om vi inte kan köra rakt (t.ex. nära väggen), kör vanlig Dijkstra som backup så programmet ej kraschar
-                dijkstra.execute(nodes.get(startNodeId));
-                path = dijkstra.getPath(nodes.get(endNodeId));
-                System.out.println("Rutt hittad, AGVs rotation ej godkänd för parkeringsmanöver");
-            }
-
-            // Spara till DataStore 
+            dijkstra.execute(nodes.get(startNodeId));
+            path = dijkstra.getPath(nodes.get(endNodeId));
             ds.currentPath = path;
-
-            if (path != null) {
-
-                // Rita ut vägen i matrisen
-                for (Vertex v : path) {
-                    int id = Integer.parseInt(v.getId());
-                    int ipos = id / ds.columns; // Rad (Y)
-                    int jpos = id - (ipos * ds.columns); // Kolumn (X)
-                    // Markera som väg (3) om det inte är start/mål (för snyggare grafik)
-                    //ds.ObstacleMatrix[jpos][ipos] = 3;
-                }
-            } else {
-                System.out.println("Ingen väg kunde hittas.");
-            }
         }
     }
 
-    //Ny väg för resa tillbaks till startnod
-public void createReturnPlan(int currentNodeId, int targetStartNodeId)
-{
-    buildGraph();
-    
-          Graph graph = new Graph(nodes, edges);
+    public void createReturnPlan(int currentNodeId, int targetStartNodeId) {
+        buildGraph(false);
+        Graph graph = new Graph(nodes, edges);
         DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(graph);
-         LinkedList<Vertex> path = null;
         dijkstra.execute(nodes.get(currentNodeId));
-                path = dijkstra.getPath(nodes.get(targetStartNodeId));
-                
-                if (path != null)
-                {
-                    ds.currentPath = path;
-                    System.out.println("Hemväg beräknad!");
-                }
-                
-}
+        ds.currentPath = dijkstra.getPath(nodes.get(targetStartNodeId));
+    }
 }
